0b4e9cbf764239a5342aad8c0c2de35ceade94a2
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2024-04-01 19:14:31 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //declaraçoa de variavel 
+        int idadePesssoa;
+        float salarioFuncionario;
+        double salario;
+        String meuEndereco;
+        String meuNome;
+
+        //atribuição de valores
+        meuEndereco = "Quadra 01";
+        idadePesssoa = 28;
+        salario = 1480.68;
+        meuNome = "isabelly moraes";
+
+        //mostra os valores armazenados
+        System.out.println("Salario = " +salario);
+        System.out.println("Endereco = " +meuEndereco);
+        System.out.println("Nome = " +meuNome);
+        System.out.println("Priogamando em");
+        System.out.println("java");
+
+        //declarando variaveis
+        int x=2;
+        System.out.printf("valor = %d\n",x);
+        
+        float numero= 2.236f;
+        System.out.printf("numero = %f\n" , numero);
+    }
+}
